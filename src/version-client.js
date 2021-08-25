241ffc369e96e5842deb4d1ad7e6f5c9a561a7a9
Dan Goslen <dgoslen@bandwidth.com>
2021-08-25 16:25:31 -0400

    Adds releasing and tests for such

--- src/version-client.js
+++ src/version-client.js
@@ -2,9 +2,8 @@ const core = require('@actions/core');
 
 module.exports.upsertVersion = async function(client, version, projectId) {
     client.getVersions(projectId).then(async (response) => {
-        // Version exists, nothing to todo
-        const names = response.map(x => x.name)
-        if (!names.includes(version)) {
+        const foundVersion = response.filter(x => x.name == version)
+        if (foundVersion.length == 0) {
             await createVersion(client, version, projectId)
         }
     }).catch(err => {
@@ -18,6 +17,7 @@ module.exports.assignVersionToIssue = async function(client, version, issueId) {
         if (!fixVersions.includes(version)) {
             const updated = [...fixVersions]
             updated.push(version)
+            issue.fields.fixVersions = updated
             updateIssue(client, issueId, issue);
         }
     }).catch(err => {
@@ -26,24 +26,36 @@ module.exports.assignVersionToIssue = async function(client, version, issueId) {
 }
 
 module.exports.releaseVersion = async function(client, version, projectId) {
-  
+    client.getVersions(projectId).then(async (response) => {
+        // Version exists, nothing to todo
+        const foundVersion = response.filter(x => x.name == version)[0]
+        const updated = {}
+        Object.assign(updated, foundVersion)
+        updated.released = true
+        await updateVersion(client, updated.id, updated)
+    }).catch(err => {
+        throw new Error('Error releasing version')
+    });
 }
 
 async function createVersion(client, version, projectId) {
     const versionBody = {
         version: version,
         projectId: projectId
-    }
-    
-    client.createVersion(versionBody).then(response => {
-        return
-    }).catch(err => {
+    }  
+    client.createVersion(versionBody).catch(err => {
         throw new Error(`Error creating version: ${err.reason}`)
     });
 }
 
 async function updateIssue(client, issueId, issue) {
-    client.updateIssue(issueId, issue).catch(function(err) {
+    client.updateIssue(issueId, issue).catch(err => {
         core.warning(`Could not add version to issue '${issueId}`)
+    });
+}
+
+async function updateVersion(client, versionId, version) {
+    client.updateVersion(versionId, version).catch(err => {
+        core.warning(`Could not release version '${versionId}`)
     });
 }
